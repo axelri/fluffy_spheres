c8b5508f97262b3a8886846467b7e4e87a60d43b
Axel Riese <axel.riese@gmail.com>
2012-12-20 11:29:37 +0100

    Update Sphere.py
    Implemented private instance variables

--- Sphere.py
+++ Sphere.py
@@ -6,22 +6,26 @@ from OpenGL.GLUT import *
 
 class Sphere:
     def __init__(self, displayListIndex):
-        self.pos = [0, 0, 0]
-        self.speed = 0.05
-        self.displayListIndex = displayListIndex
+        # _<some> = private variables
+        # only internal methods shall call these
+        self._xPos = 0
+        self._yPos = 0
+        self._zPos = 0
+        self._speed = 0.05
+        self._displayListIndex = displayListIndex
 
     def update(self):
         keyState = pygame.key.get_pressed()
         # D positive, a negative, 0 if not pressed
-        x_dir = keyState[K_d] - keyState[K_a]
-        z_dir = keyState[K_w] - keyState[K_s]
-        self.pos[0] += x_dir * self.speed
-        self.pos[2] += z_dir * self.speed
+        xDir = keyState[K_d] - keyState[K_a]
+        zDir = keyState[K_w] - keyState[K_s]
+        self.xPos += x_dir * self.speed
+        self.zPos += z_dir * self.speed
 
         self.draw()
 
     def draw(self):
         glPushMatrix()
-        glTranslate(self.pos[0], self.pos[1], self.pos[2])
-        glCallList(self.displayListIndex)
+        glTranslate(self._xPos, self._yPos, self._zPos)
+        glCallList(self._displayListIndex)
         glPopMatrix()
